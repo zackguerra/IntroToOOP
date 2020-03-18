b256e38dc2dcba241c6cec8cfd03eba5038a466f
Zack Guerra <ezequiasguerra@yahoo.com.br>
2020-03-18 08:31:55 -0700

    Update files

--- src/collections/list/MyLinkedList.java
+++ src/collections/list/MyLinkedList.java
@@ -1,243 +1,113 @@
 package collections.list;
 
-import java.util.*;
+import java.util.Objects;
 
-// "generic" type <E> : Element
-public class MyArrayList<E> implements List<E> {
-    private static final int DEFAULT_CAPACITY = 10;
-    private Object[] elementData;
+// Doubly-Linked list
+public class MyLinkedList<E> {
     private int size;
+    private Node head, tail;
 
-    public MyArrayList() {
-        elementData = new Object[DEFAULT_CAPACITY];
-    }
-
-    public MyArrayList(int initialCapacity) {
-        if (initialCapacity >= 0) {
-            elementData = new Object[initialCapacity];
-        } else {
-            throw new IllegalArgumentException("Illegal Capacity: " + initialCapacity);
+    private static class Node<E> {
+        E data;
+        Node next;
+        Node prev;
+        Node(E data, Node next, Node prev) {
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
         }
     }
 
-    // <? extends E> -> any type that extends E
-    public MyArrayList(Collection<? extends E> c) {
-        elementData = c.toArray();
-    }
-
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
     public boolean isEmpty() {
         return size == 0;
     }
 
-    @Override
-    public boolean contains(Object o) {
-        return indexOf(o) >= 0;
+    public int size() {
+        return size;
     }
 
-    @Override
-    public Iterator<E> iterator() {
-        return null;
-    }
-
-    @Override
-    public Object[] toArray() {
-        return Arrays.copyOf(elementData, size);
-    }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        // TODO
-        return null;
-    }
-
-    @Override
-    public boolean add(E e) {
-        if (size == elementData.length) {
-            // grow (increases by 50%)
-            elementData = grow(size + 1);
-        }
-        elementData[size] = e;
-        size++;
-        return true;
-    }
-
-    private Object[] grow(int minCapacity) {
-        return elementData = Arrays.copyOf(elementData, newCapacity(minCapacity));
-    }
-
-    /*
-    The maximum size of array to allocate.
-    Attempts to allocate larger arrays "may" result in OutOfMemoryOrder
-     */
-    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
-
-    /**
-     * @param minCapacity the desired minimum capacity
-     * @return
-     */
-    private int newCapacity(int minCapacity) {
-        int oldCapacity = elementData.length;
-        int newCapacity = oldCapacity + (oldCapacity >> 1);
-        if (newCapacity <= minCapacity) {
-            if (minCapacity < 0 || minCapacity > MAX_ARRAY_SIZE) {
-                throw new OutOfMemoryError("integer overflow");
-            }
-            return minCapacity;
-        }
-        return (newCapacity <= MAX_ARRAY_SIZE) ? newCapacity : Integer.MAX_VALUE;
-    }
-    public void test(){
-
-    }
-    @Override
-    public boolean remove(Object o) {
-        for(int i = 0; i<this.elementData.length;i++){
-            if (this.elementData[i] == o){
-                this.elementData[i]=null;
+    public boolean contains(E data) {
+        Node<E> myNode = head;
+        for (int i = 0; i < size; i++) {
+            if (Objects.equals(myNode.data, data)) {
                 return true;
             }
+            //move to the next one
+            myNode = myNode.next;
         }
         return false;
     }
 
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        boolean find = false;
-        for (Object obj : c){
-            for (int i = 0; i < this.elementData.length;i++){
-                if (this.elementData[i]==obj){
-                    find = true;
-                }else{
-                    find = false;
-                }
+    public void add(E data) {
+        if(size != 0){
+            Node newNode = new Node<E>(data,null,tail);
+            tail.next = newNode;
+            tail = newNode;
+        }else{
+            head = new Node<E>(data, null, null);
+            tail = head;
+        }
+        size++;
+    }
+
+    public void add(int i, E data) {
+        if (i == size) {
+            add(data);
+        } else {
+            Node currentNode = get(i);
+            Node newNode = new Node<E>(data, currentNode, currentNode.prev);
+            if (currentNode.prev != null) {
+                currentNode.prev.next = newNode;
             }
-            if (!find)
-                return false;
-        }
-        return true;
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends E> c) {
-        for(Object o : c){
-            add((E) o);
-        }
-        return false;
-    }
-
-    @Override
-    public boolean addAll(int index, Collection<? extends E> c) {
-        for(Object o : c){
-            add(index,(E)o);
-        }
-        return false;
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        for (int i = 0 ; i < this.elementData.length; i++){
-            if (this.elementData[i] == c)
-                this.elementData[i] = null;
-            if (this.elementData[i] == null)
-                break;
-        }
-        this.size = 0;
-        return false;
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        // TODO
-        return false;
-    }
-
-    @Override
-    public void clear() {
-        Object[] obj = this.elementData;
-        int numer = this.size;
-        this.size = 0;
-        for(int i = this.size; i < numer; ++i) {
-            obj[i] = null;
-        }
-    }
-
-    @Override
-    public E get(int index) {
-        if (index < 0 || index >= size) {
-            throw new ArrayIndexOutOfBoundsException("Index " + index + ", size: " + size);
-        }
-        return (E) elementData[index];
-    }
-
-    @Override
-    public E set(int index, E element) {
-        if (index < 0 || index >= size) {
-            throw new ArrayIndexOutOfBoundsException("Index " + index + ", size: " + size);
-        }
-        E oldValue = (E) elementData[index];
-        elementData[index] = element;
-        return oldValue;
-    }
-
-    @Override
-    public void add(int index, E element) {
-        // TODO
-    }
-
-    @Override
-    public E remove(int index) {
-        this.elementData[index]= null;
-        for (int i= index;i < this.elementData.length;i++){
-            if (!(i+1 < this.elementData.length) && (this.elementData[i+1] != null)){
-                this.elementData[i] = this.elementData[i+1];
-            }else{
-                break;
+            currentNode.prev = newNode;
+            if (i == 0) {
+                head = newNode;
             }
+            size++;
         }
-        return null;
     }
 
-    @Override
-    public int indexOf(Object o) {
-        for(int i = 0; i < this.elementData.length;i++){
-            if (this.elementData[i]== o)
-                return i;
+    public E remove(int i) {
+        Node myNode = get(i);
+        size--;
+        if (myNode.prev == null) {
+            head = head.next;
+            if (head != null) {
+                head.prev = null;
+            }
+        } else {
+            myNode.prev.next = myNode.next;
         }
-        return -1;
-    }
-
-    @Override
-    public int lastIndexOf(Object o) {
-        for(int i = this.elementData.length; i >= 0;i--){
-            if (this.elementData[i]== o)
-                return i;
+        if (myNode.next == null) {
+            tail = tail.prev;
+            if (tail != null) {
+                tail.next = null;
+            }
+        } else {
+            myNode.next.prev = myNode.prev;
         }
-        return -1;
+        return (E) myNode.data;
     }
 
-    @Override
-    public ListIterator<E> listIterator() {
-        return null;
-    }
-
-    @Override
-    public ListIterator<E> listIterator(int index) {
-        return null;
-    }
-
-    @Override
-    public List<E> subList(int fromIndex, int toIndex) {
-        List<E> newReturn = new ArrayList<>();
-        int j = 0;
-        for (int i = fromIndex ; i< toIndex; i++){
-            newReturn.add((E) this.elementData[i]);
+    public Node get(int i) {
+        Node<E> currentNode = head;
+        for (int j = 0; i < i; i++) {
+            currentNode = currentNode.next;
         }
-        return newReturn;
+        return currentNode;
+    }
+
+    public E set(int i, E newData) {
+        Node<E> node = get(i);
+        E old = node.data;
+        node.data = newData;
+        return old;
+    }
+
+    @Override
+    public String toString() {
+        // TODO
+        // ex) "Obj0"=="Obj1"==null
+        return super.toString();
     }
 }
